cbbcb720da7228c8ceeaf70be795a1bafe409a1c
ibtesam <ibtesam-almohsen@hotmail.com>
2023-04-16 07:17:19 +0300

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
 import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenre";
+import getCroppedImageURL from "../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((gerne) => (
-        <li key={gerne.id}>{gerne.name}</li>
+        <ListItem key={gerne.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageURL(gerne.image_background)}
+            ></Image>
+            <Text fontSize="lg">{gerne.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
